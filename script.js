374096592ebb415b8122dd3089ab5451d9143a25
Mustafa-muftah <70711751+Mustafa-muftah@users.noreply.github.com>
2021-05-31 22:03:31 +0200

    Last

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const move = (img) => {
   let posValue = parseInt(pos);
   img.style.transition = "left 1s ease-out ";
   img.style.left = posValue + 50 + "px";
-  if (posValue + 50 <= 1390) {
+  if (posValue + 50 <= 1400) {
     posValue++;
   } else {
     posValue = -2000;
