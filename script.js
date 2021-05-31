4c3602e144c274c940230eac60c4f11102dec1e1
Mustafa-muftah <70711751+Mustafa-muftah@users.noreply.github.com>
2021-05-31 05:08:26 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const imageOne = document.getElementById("imgOne");
+const imageTwo = document.getElementById("imgTwo");
+const imageThree = document.getElementById("imgThree");
+const imageFour = document.getElementById("imgFour");
+const imageFive = document.getElementById("imgFive");
+const container = document.querySelector(".container");
+const button = document.getElementById("btn");
+const imageArray = [imageOne, imageTwo, imageThree, imageFour, imageFive];
+
+window.onload = () => {
+  imageArray.map((img) =>
+    setInterval(function () {
+      move(img);
+    }, 300)
+  );
+};
+const move = (img) => {
+  const style = getComputedStyle(img);
+  let pos = style.left;
+  let posValue = parseInt(pos);
+  img.style.transition = "left 1s ";
+  img.style.left = posValue + 50 + "px";
+  if (posValue < 1400) {
+    posValue++;
+  } else {
+    posValue = -2000;
+    img.style.left = posValue + 50 + "px";
+  }
+};
